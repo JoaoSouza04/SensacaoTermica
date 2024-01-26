687c7bc1bfde8a082f85b0a105ebdbb4be46a4f8
João Pedro <joaosvieira04@gmail.com>
2024-01-26 14:33:53 -0300

    Atualizando o path das rotas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SensacaoTermicaComponent } from './sensacao-termica/sensacao-termica.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'weather', component: SensacaoTermicaComponent }
 ];
 
